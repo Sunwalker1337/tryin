42847353956df696763141bc6980202e4300671f
Oleksii Bukhanevych <holmivechnihpesen@gmail.com>
2022-08-09 16:55:35 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// let banana = ingredients.map(()=> document.createElement('li'));
+// console.log(banana)
+
+let cherry = document.createElement('li');
+cherry.textContent ='1';
+
+console.log(cherry);
+
+
+let liItems = ingredients.map(
+(element) => {
+let variable = document.createElement('li');
+variable.textContent = `${element}`;
+return variable
+}
+)
+console.log(liItems)
+
+let list = document.getElementById('ingredients');
+
+list.prepend(...liItems);
