618fc909e6a4554f9814c12ae059f373f426a6ea
Oleksii Bukhanevych <holmivechnihpesen@gmail.com>
2022-08-10 19:57:49 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//variables
+let body = document.querySelector('body');
+let colorBtn = document.querySelector('.change-color');
+let colorName =  document.querySelector('.color')
+let colorRandomizer =function getRandomHexColor1() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+colorBtn.addEventListener('click', skittles)
+
+function skittles(){
+let color = colorRandomizer();
+body.style.backgroundColor = color;
+colorName.innerText = color;
 }
