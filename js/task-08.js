2a7cd7f647d7737af888f57a947f92532c3399e7
Oleksii Bukhanevych <holmivechnihpesen@gmail.com>
2022-08-10 19:32:58 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,43 @@
+//variables
+let form = document.querySelector('.login-form');
+let formData =  new FormData(form);
+
+let email = document.querySelector('[type="email"]')
+let password = document.querySelector('[type="password"]')
+
+
+console.log(formData.entries())
+
+// function
+function collector (){
+    event.preventDefault(); 
+    
+    formData.set('email', email.value)
+    formData.set('password', password.value)
+
+    if(email.value === ""){
+        alert('All fields must be filled.')
+    }
+    if(password.value === ""){
+        alert('All fields must be filled.')
+    }
+
+
+    let data ={
+        email : formData.get('email'),
+        password : formData.get('password')
+    }
+
+    console.log(data)
+
+    form.reset()
+};
+
+
+// event listeners
+form.addEventListener('submit', collector)
+
+
+
+
 
